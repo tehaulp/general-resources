2479192055c9a5fa5dcbe6da1380fd3794201a96
tehaulp <fauatehau02gmail.com>
2024-12-29 21:05:09 -1000

    Added app.js file for natiraa

---
+++ natiraa/app.js
@@ -0,0 +1,13 @@
+function toggleUserDropdown(event) {
+  const dropdownMenu = document.getElementById('dropdown-user');
+  dropdownMenu.classList.toggle('hidden');
+}
+
+document.addEventListener('click', function(event) {
+  const dropdownMenu = document.getElementById('dropdown-user');
+  const dropdownButton = document.querySelector('[data-dropdown-toggle="dropdown-user"]');
+  
+  if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.classList.add('hidden');
+  }
+});
