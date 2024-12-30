6b278f210ebf76d6093984670bae51ab6b1391c3
tehaulp <fauatehau02gmail.com>
2024-12-29 21:34:38 -1000

    Modified app.js

--- natiraa/app.js
+++ natiraa/app.js
@@ -1,1 +1,13 @@
-console.log("Le fichier JavaScript a bien été chargé !");
+function toggleUserDropdown(event) {
+  const dropdownMenu = document.getElementById('dropdown-user');
+  dropdownMenu.classList.toggle('hidden');
+}
+
+document.addEventListener('click', function(event) {
+  const dropdownMenu = document.getElementById('dropdown-user');
+  const dropdownButton = document.querySelector('[data-dropdown-toggle="dropdown-user"]');
+  
+  if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+    dropdownMenu.classList.add('hidden');
+  }
+});
