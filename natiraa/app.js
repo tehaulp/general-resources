10d7cd67a26f7797fa98f62495f725d1ce721b96
tehaulp <fauatehau02gmail.com>
2024-12-29 21:20:41 -1000

    Modified app.js

--- natiraa/app.js
+++ natiraa/app.js
@@ -1,17 +1,1 @@
-function toggleUserDropdown(event) {
-  const dropdownMenu = document.getElementById('dropdown-user');
-  dropdownMenu.classList.toggle('hidden');
-}
-
-document.addEventListener('click', function(event) {
-  const dropdownMenu = document.getElementById('dropdown-user');
-  const dropdownButton = document.querySelector('[data-dropdown-toggle="dropdown-user"]');
-  
-  if (!dropdownButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
-    dropdownMenu.classList.add('hidden');
-  }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  alert('Hello');
-});
+console.log("Le fichier JavaScript a bien été chargé !");
